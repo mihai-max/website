f78469370d7c8d9be4bc4d48bbdd362de7db5c33
mihai-max <55044519+mihai-max@users.noreply.github.com>
2024-06-05 21:44:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-VANTA.HALO({
-  el: "#vanta-bg",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00
+document.addEventListener('DOMContentLoaded', () => {
+  VANTA.HALO({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.0,
+    scaleMobile: 1.0
+  });
 });
