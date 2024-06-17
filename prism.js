8a0d8b986d2087642a94a75d21af315a59ae795a
mihai-max <55044519+mihai-max@users.noreply.github.com>
2024-06-17 10:11:18 +0200

    Wrap prism.js in JSX fragment

--- prism.js
+++ prism.js
@@ -1,3 +1,2 @@
 <!-- Link to Prism.js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/prism.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/themes/prism.min.css" />
+    <><script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/prism.min.js"></script><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/themes/prism.min.css" /></>
